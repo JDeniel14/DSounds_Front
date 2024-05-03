b437468c3d39c37128fe08ab1766cb8c34663763
Deniel <128909477+JDeniel14@users.noreply.github.com>
2024-05-03 18:48:35 +0200

    added lista Discos

--- src/app/Components/TiendaComponents/ListaDiscosComponent/listado-discos.component.ts
+++ src/app/Components/TiendaComponents/ListaDiscosComponent/listado-discos.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DiscoComponent } from '../DiscoComponent/disco/disco.component';
+import { RouterLink } from '@angular/router';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import IDisco from '../../../Models/Disco';
+import { RestNodeService } from '../../../Services/rest-node.service';
 
 @Component({
   selector: 'app-listado-discos',
   standalone: true,
-  imports: [],
+  imports: [DiscoComponent, RouterLink, NzSkeletonModule],
   templateUrl: './listado-discos.component.html',
   styleUrl: './listado-discos.component.css'
 })
-export class ListadoDiscosComponent {
+export class ListadoDiscosComponent implements OnInit{
+  public Discos:IDisco[]= [];
+
+  /**
+   *
+   */
+  constructor(private restSvc : RestNodeService) {
+
+  }
+  ngOnInit(): void {
+
+
+
+    this.ObtenerDiscos();
+
+
+
+}
+
+async ObtenerDiscos(){
+
+  let resp = await this.restSvc.ObtenerDiscos()
+
+  if(resp.codigo == 0){
+    this.Discos = resp.otrosdatos;
+
+    
+  }else{
+    console.log('error obteniendo discos...')
+  }
+}
 
 }
